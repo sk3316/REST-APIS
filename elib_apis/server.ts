fe7ebae3b74dc0f837ccef067b9375e3f55611a9
sk3316 <sk3316@srmist.edu.in>
2024-10-06 12:31:54 +0530

    add config;

--- elib_apis/server.ts
+++ elib_apis/server.ts
@@ -1,9 +1,12 @@
 import app from "./src/app";
+import { config } from "./src/config";
+
+
 
 const startServer = () =>{
 
 
-    const port = process.env.PORT || 3000;
+    const port = config.port || 3000;
 
     app.listen(port, () =>{
         console.log(`Listening on port: ${port}`);
